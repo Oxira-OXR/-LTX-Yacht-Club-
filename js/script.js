2deedfde6c5ac4d22556295bec8b8edd815c7007
Oxira-OXR <oxira.oxr@gmail.com>
2025-09-28 12:24:30 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
+const hamburger = document.getElementById('hamburger');
+const sideMenu = document.getElementById('sideMenu');
 
+hamburger.addEventListener('click', () => {
+  sideMenu.classList.toggle('show');
+});
